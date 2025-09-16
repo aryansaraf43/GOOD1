7c2a13293ca685508f3a8d62421a7e1a6d748416
aryansaraf43 <sarafneha48@gmail.com>
2025-09-16 14:22:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIO(server);
 
-app.use(express.static(__dirname));
-
-io.on('connection', (socket) => {
-    console.log('New user connected');
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Handle socket connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // Listen for chat messages
+  socket.on('chat message', (data) => {
+    // Broadcast the message to all connected clients
+    io.emit('chat message', {
+      username: data.username,
+      message: data.message,
+      time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    });
+  });
+  
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
